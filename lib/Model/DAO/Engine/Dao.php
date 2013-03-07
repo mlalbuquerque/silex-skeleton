71677292bfae9e7ace74ca48cbfb7bfab4c0c6bf
Marcio Albuquerque <marcio.lima.albuquerque@gmail.com>
2013-03-07 07:46:38 -0300

    New DAO methods to simplify some queries!

--- lib/Model/DAO/Engine/Dao.php
+++ lib/Model/DAO/Engine/Dao.php
@@ -5,41 +5,61 @@ namespace Model\DAO\Engine;
 abstract class Dao
 {
     
-    protected $db, $qb;
+    protected $db, $qb, $cols;
 
     public function __construct(\Doctrine\DBAL\Connection $db)
     {
         $this->db = $db;
         $this->qb = $db->createQueryBuilder();
+        $this->cols = null;
     }
     
     abstract public function findAll(array $options = array());
     abstract public function findOne(array $options = array());
     abstract public function getTotal(array $options = array());
     abstract public static function getColumns();
-    abstract public function getTableName();
-    abstract public function getTableAlias();
+    abstract protected function getTableName();
+    abstract protected function getTableAlias();
+    
+    public function setColumns(array $cols)
+    {
+        $old_cols = static::getColumns();
+        $new_cols = array();
+        foreach ($cols as $col)
+            if (in_array($col, $old_cols))
+                $new_cols[] = $col;
+
+        $this->cols = $new_cols;
+    }
     
     protected function getColumn($index)
     {
-        $cols = static::getColumns();
+        $cols = empty($this->cols) ? static::getColumns() : $this->cols;
         $col = (array_key_exists($index, $cols)) ? $cols[$index] : $index;
         return $col;
     }
     
     protected static function getSelectColumns($prefix = null)
     {
+        $selectedCols = empty($this->cols) ? static::getColumns() : $this->cols;
         $cols = (!empty($prefix)) ? array_map(function ($value) use ($prefix) {
             return $prefix.'.'.$value;
-        }, static::getColumns()) : static::getColumns();
+        }, $selectedCols) : $selectedCols;
         return implode(', ', $cols);
     }
     
-    protected function prepareSelectFrom()
+    /**
+     * Prepares SELECT and FROM clauses to query using \Doctrine\DBAL\Query\QueryBuilder
+     * @param array $append Array of other columns to append to SELECT
+     */
+    protected function prepareSelectFrom(array $append = array())
     {
         $this->qb->resetQueryParts();
-        $this->qb->select(self::getSelectColumns())
+        $this->qb
+             ->select(self::getSelectColumns())
              ->from($this->getTableName(), $this->getTableAlias());
+        if (!empty($append))
+            $this->qb->addSelect(implode(', ', $append));
     }
     
     protected function entityFromArray(array $values)
