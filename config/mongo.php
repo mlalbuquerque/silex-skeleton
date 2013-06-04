e14d653bbdfc7f0009562e3c38bcd89ceb7fea25
Marcio Albuquerque <marcio.lima.albuquerque@gmail.com>
2013-06-04 09:16:02 -0300

    MongoDB config file

---
+++ config/mongo.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'username' => 'admin',
+    'password' => '12345',
+    'hosts'    => array('localhost'),
+    'ports'    => array(27017),
+    'database' => 'central_de_pacotes'
+);
